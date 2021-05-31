02d1fc975db8f09ce690e8dc70a07591d8d75750
chuanjiaSUN <825927681@qq.com>
2021-05-31 16:07:52 +0800

    Algorithms learning process

---
+++ src/tree/day18/exercise669.java
@@ -0,0 +1,34 @@
+package tree.day18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sunchuanjia
+ * @Description
+ * @create 2021-05-31 15:45
+ */
+public class exercise669 {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return root;
+        if (root.val > high) return trimBST(root.left, low, high);
+        if (root.val < low) return trimBST(root.right, low, high);
+        root.left = trimBST(root.left, low, high);
+        root.right= trimBST(root.right, low, high);
+        return root;
+    }
+
+
+}
