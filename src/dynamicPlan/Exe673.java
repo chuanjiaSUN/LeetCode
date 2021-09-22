13f748b95357dffea4df128aa6b96a3913970e00
chuanjiaSUN <825927681@qq.com>
2021-09-22 16:00:28 +0800

    long subSequence

--- src/dynamicPlan/Exe673.java
+++ src/dynamicPlan/Exe673.java
@@ -2,7 +2,6 @@ package dynamicPlan;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UnknownFormatConversionException;
 
 /**
  * @author sunchuanjia
@@ -105,4 +104,39 @@ public class Exe673 {
         }
         return l;
     }
+
+    public int findNumberOfLIS4(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] cnt = new int[n];
+        int maxLen = 0, ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = 1;
+            cnt[i] = 1;
+            for (int j = 0; j <= i ;j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    if (dp[j] + 1 > dp[i])
+                    {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    }else if (dp[j] + 1 == dp[i])
+                    {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen)
+            {
+                maxLen = dp[i];
+                ans = cnt[i];
+            }else if (dp[i] == maxLen){
+                ans += cnt[i];
+            }
+        }
+        return ans;
+    }
+
 }
