af7984e48fddfba8123f5bb5dc1b0d5fd6d00ae4
chuanjiaSUN <825927681@qq.com>
2022-01-18 14:56:03 +0800

    fix

--- src/divideAndConqure/Exe190.java
+++ src/divideAndConqure/Exe190.java
@@ -9,7 +9,7 @@ public class Exe190 {
     public int reverseBits(int n) {
         int rev = 0;
         for (int i = 0; i < 32 && n != 0 ; i++){
-            rev = (n & 1) << (31 - i);
+            rev |= (n & 1) << (31 - i);
             n >>>= 1;
         }
         return rev;
