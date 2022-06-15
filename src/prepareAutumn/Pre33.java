ccb55e3510297a118363d0ac772ac88cc04fe1ea
chuanjiaSUN <825927681@qq.com>
2022-06-15 23:00:31 +0800

    prepare for autumn day5

---
+++ src/prepareAutumn/Pre33.java
@@ -0,0 +1,38 @@
+package prepareAutumn;
+
+/**
+ * @author sunchuanjia
+ * @Description
+ * @create 2022-06-15 21:18
+ */
+public class Pre33 {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int len = nums.length;
+
+        int left = 0;
+        int right = len - 1;
+        while (left <= right){
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (nums[0] <= nums[mid]){
+                if (nums[0] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if (nums[mid] < target && target <= nums[len - 1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
